0b169c777067c6bb55589b905d2ffb19b792a025
tomasamori <tomas.amori@gmail.com>
2024-11-06 21:48:14 +0000

    feat: add generic Stack class with type restrictions and basic push, pop and size methods

---
+++ src/allowedTypes.ts
@@ -0,0 +1,1 @@
+export type AllowedTypes = boolean | string | number;
---
+++ src/stack.ts
@@ -0,0 +1,21 @@
+import { AllowedTypes } from './allowedTypes';
+
+class Stack<T extends AllowedTypes> {
+
+    protected items: T[] = [];
+
+    push(item: T): void {
+        this.items.push(item);
+        console.log(this.items);
+    }
+
+    pop(): void {
+        this.items.pop();
+        console.log(this.items);
+    }
+
+    size(): void {
+        console.log("Elementos contenidos en la pila:", this.items.length);
+    }
+
+}
